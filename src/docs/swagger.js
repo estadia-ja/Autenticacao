c9c11f3da622d7adcc11d5f343e8b5a6a2238f44
pkbceira03 <cabeceira2003@gmail.com>
2025-11-13 15:21:38 -0300

    fix: adiciona a url de produção

--- src/docs/swagger.js
+++ src/docs/swagger.js
@@ -1,41 +1,48 @@
 import swaggerJSDoc from 'swagger-jsdoc';
 
+const isProduction = process.env.NODE_ENV === 'production';
+
+const productionServer = {
+  url: 'https://estadia-ja-auth.onrender.com', 
+  description: 'Servidor de Produção (Render)',
+};
+
+const developmentServer = {
+  url: 'http://localhost:3001',
+  description: 'Servidor de Desenvolvimento',
+};
+
 const swaggerDefinition = {
-    openapi: '3.0.0',
-    info: {
-        title: 'Serviço de Autenticação - Estadia Já',
-        version: '1.0.0',
-        description: 'Documentação da API de autenticação (login)' 
+  openapi: '3.0.0',
+  info: {
+    title: 'Serviço de Autenticação - Estadia Já',
+    version: '1.0.0',
+    description: 'Documentação da API de autenticação (login)',
+  },
+  servers: isProduction ? [productionServer, developmentServer] : [developmentServer, productionServer],
+  components: {
+    securitySchemes: {
+      bearerAuth: {
+        type: 'http',
+        scheme: 'bearer',
+        bearerFormat: 'JWT',
+      },
     },
-    servers: [
-        {
-            url: 'http://localhost:3001', 
-            description: 'Servidor de desenvolvimento'
-        },
-    ],
-    components:{
-        securitySchemes:{
-            bearerAuth: {
-                type: 'http',
-                scheme: 'bearer',
-                bearerFormat: 'JWT',
-            }
-        }
+  },
+  tags: [
+    {
+      name: 'Autenticação',
+      description: 'Rota login de usuário',
     },
-    tags: [
-        {
-            name: 'Autenticação', 
-            description: 'Rota login de usuário'
-        }
-    ],
+  ],
 };
 
 const options = {
-    swaggerDefinition,
-    apis: [
-        './src/auth/routes.js' 
-    ],
-  };
+  swaggerDefinition,
+  apis: [
+    './src/auth/routes.js',
+  ],
+};
 
 const swaggerSpec = swaggerJSDoc(options);
 
